e610c81d058f69ad3667ccacf07643919b371c59
sheenazien8 <sheenazien08@gmail.com>
2020-12-08 01:13:39 +0700

    change icon

--- config/menu.php
+++ config/menu.php
@@ -15,63 +15,63 @@ return [
         'active' => ['nasabah.index', 'nasabah.create', 'nasabah.edit', 'nasabah.show'],
         'menu_title' => 'Nasabah',
         'display_for' => ['admin'],
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-user-friends'
     ],
     [
         'route' => 'user.index',
         'active' => ['user.index', 'user.create', 'user.edit', 'user.show'],
         'menu_title' => 'User',
         'display_for' => ['admin'],
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-users'
     ],
     [
         'route' => 'item.index',
         'active' => ['item.index', 'item.create', 'item.edit', 'item.show'],
         'display_for' => ['admin'],
         'menu_title' => 'Item',
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-pastafarianism'
     ],
     [
         'route' => 'pic.index',
         'active' => ['pic.index', 'pic.edit', 'pic.create', 'pic.show'],
         'menu_title' => 'Pic',
         'display_for' => ['admin'],
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-user'
     ],
     [
         'route' => 'today-pic.index',
         'active' => ['today-pic.index', 'today-pic.edit', 'today-pic.create', 'today-pic.show'],
         'display_for' => ['admin'],
         'menu_title' => 'Today Pic',
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-user-cog'
     ],
     [
         'route' => 'saving.index',
         'active' => ['saving.index', 'saving.show'],
         'display_for' => ['admin'],
         'menu_title' => 'Saving',
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-piggy-bank'
     ],
     [
         'route' => 'transaction.index',
         'display_for' => ['nasabah', 'admin'],
         'active' => ['transaction.index', 'transaction.edit', 'transaction.show'],
         'menu_title' => 'Transaction',
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-money-check-alt'
     ],
     [
         'route' => 'content.index',
         'display_for' => ['nasabah', 'admin'],
         'active' => ['content.index', 'content.show', 'content.create', 'content.edit'],
         'menu_title' => 'Content',
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-cubes'
     ],
     [
         'route' => 'activity.index',
         'display_for' => ['nasabah', 'admin'],
         'active' => ['activity.index', 'activity.show', 'activity.create', 'activity.edit'],
         'menu_title' => 'Activity',
-        'icon' => 'fas fa-th'
+        'icon' => 'fas fa-th-list'
     ],
     [
         'menu-header' => 'Setting',
