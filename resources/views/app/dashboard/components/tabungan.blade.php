c3f29b2f85c5a838ae15f9f54c75f768fa0f10fb
sheenazien8 <sheenazien08@gmail.com>
2020-12-08 22:09:47 +0700

    userseeder

---
+++ resources/views/app/dashboard/components/tabungan.blade.php
@@ -0,0 +1,16 @@
+<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+  <div class="card card-statistic-1">
+    <div class="card-icon bg-primary">
+      <i class="far fa-user"></i>
+    </div>
+    <div class="card-wrap">
+      <div class="card-header">
+        <h4>@lang('app.dashboard.total_admin')</h4>
+      </div>
+      <div class="card-body">
+        {{ $admin }}
+      </div>
+    </div>
+  </div>
+</div>
+
